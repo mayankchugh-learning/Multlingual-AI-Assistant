25fc36fb15beb3d3f3ec98745bf0c7abb5d617b7
mayankchugh-learning <mayankchugh.learning@gmail.com>
2024-04-10 23:35:53 +0800

    updated template.py and created files and folders structure

--- template.py
+++ template.py
@@ -2,6 +2,34 @@ import os
 import logging
 from pathlib import Path
 
-
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
+project_name = "multlingual"
+
+list_of_files = [
+    f"src/__init__.py",
+    f"src/helper.py",
+    f"research/trials.ipynb",
+    ".env",
+    "app.py",
+    "requirements.txt",
+    "setup.py"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
